0d5876ce9d277f2fc3f11d4a464e6b452c04d01a
dhhagan <dhhagan@users.noreply.github.com>
2024-02-12 16:29:20 +0000

    deploy: f7952db12f664abd2a032d798e57f052f6da6d1b

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Tests suite for `aeromancy`."""
-
-from pathlib import Path
-
-TESTS_DIR = Path(__file__).parent
-FIXTURES_DIR = TESTS_DIR / "fixtures"
--- tests/test_import.py
+++
@@ -1,8 +0,0 @@
-"""Test Aeromancy is importable."""
-
-import aeromancy
-
-
-def test_import_aeromancy() -> None:
-    """Ensure aeromancy package can be imported."""
-    print("Contents:", dir(aeromancy))
